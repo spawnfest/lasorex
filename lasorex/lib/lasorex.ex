72c644dcf2c705af5c854eb608dcdb034fb9211a
Alan Ficagna <alan.ficagna@derivco.se>
2019-09-21 09:18:00 +0200

    Initial commit

---
+++ lasorex/lib/lasorex.ex
@@ -0,0 +1,18 @@
+defmodule Lasorex do
+  @moduledoc """
+  Documentation for Lasorex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Lasorex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ lasorex/test/lasorex_test.exs
@@ -0,0 +1,8 @@
+defmodule LasorexTest do
+  use ExUnit.Case
+  doctest Lasorex
+
+  test "greets the world" do
+    assert Lasorex.hello() == :world
+  end
+end
